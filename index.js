25770ffbfccbdbd1fed71879fb93064be8e5252b
Prashob padmanabhan <prashobhpadmanabhan@gmail.com>
2022-12-07 11:16:45 +0530

    updation

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ const Port = 4200;
 app.use(express.json());
 app.use('/api/v1', userRoute);
 
-// app.get('/', (req, res) => {
-//     res.send("hello world")
-// })
+
 
 const start = async () => {
     try {
